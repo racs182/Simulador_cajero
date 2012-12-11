87cdf8d94b7bf47d2968ba074d5d96d33a407997
racs182 <racs182@gmail.com>
2012-12-11 17:36:28 -0430

    Agregando archivo seleccionoperaciones y corrigiendo iconmoon

---
+++ recursos/css/icomoon76709/lte-ie7.js
@@ -0,0 +1,31 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-arrow-right' : '&#x44;',
+			'icon-arrow-right-2' : '&#x64;',
+			'icon-arrow-left' : '&#x69;',
+			'icon-checkmark' : '&#x42;',
+			'icon-cancel' : '&#x78;',
+			'icon-enter' : '&#x65;',
+			'icon-arrow-left-2' : '&#x48;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
